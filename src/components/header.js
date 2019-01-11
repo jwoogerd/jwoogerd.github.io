db1c5a1593e7fbbdb2cf66cee3a20df4de75e40d
Jayme Woogerd <jayme@plethora.com>
2019-01-11 11:55:38 -0500

    Update tagline

--- src/components/header.js
+++ src/components/header.js
@@ -16,7 +16,7 @@ class Header extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      tagline: 'Champion',
+      tagline: 'Washed up athlete',
       change: false,
     }
   }
